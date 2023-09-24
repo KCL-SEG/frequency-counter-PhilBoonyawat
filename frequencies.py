6ee75bf95ca8785f8f2c21f5569c3df880527c43
Boonyawat Ariyawatkul <boonyawat.ariyawatkul@kcl.ac.uk>
2023-09-24 21:12:09 +0100

    Add contents to frequencies function in frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,12 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if frequencies.get(str(item)) != None:
+            frequencies[str(item)] = frequencies[str(item)] + 1 
+        else:
+            frequencies[str(item)] = 1
+
     return frequencies
+
+print(frequencies([1,1,1,1,4,2,3]))
